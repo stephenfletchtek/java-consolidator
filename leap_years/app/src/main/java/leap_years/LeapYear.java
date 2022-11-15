70d3e9ebd27454e0f139ff5c818716cf400c126b
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-15 13:37:12 +0000

    refactor LeapYear.java with IntelliJ suggestions and capture argument if run

--- leap_years/app/src/main/java/leap_years/LeapYear.java
+++ leap_years/app/src/main/java/leap_years/LeapYear.java
@@ -1,17 +1,18 @@
 package leap_years;
 
 public class LeapYear {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         LeapYear leapYear = new LeapYear();
+        if (args.length > 0) {
+            Integer year = Integer.valueOf(args[0]);
+            System.out.println(leapYear.isLeapYear(year));
+        }
     }
 
     public Boolean isLeapYear(Integer year) {
         if (year % 400 == 0) {
             return true;
         }
-        if (year % 4 == 0 && year % 100 != 0) {
-            return true;
-        }
-        return false;
+        return year % 4 == 0 && year % 100 != 0;
     }
 }
