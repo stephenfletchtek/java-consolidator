7f81f93522e62a5b43f5e803ca0585dde6b1f3cc
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-16 15:51:54 +0000

    pass first test after refactor

--- middle_letters/app/src/main/java/middle_letters/MiddleLetter.java
+++ middle_letters/app/src/main/java/middle_letters/MiddleLetter.java
@@ -2,6 +2,13 @@ package middle_letters;
 
 public class MiddleLetter {
     public String get_middle(String word) {
-        return "es";
+        int startPoint = word.length()/2 -1 ;
+        StringBuilder middle = new StringBuilder();
+        middle.append(word.charAt(startPoint));
+
+        if (word.length() % 2 == 0) {
+            middle.append(word.charAt(startPoint + 1));
+        }
+        return middle.toString();
     }
 }
