0834a8b4a459c663897ff1342abb85c79fa3b5cf
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-16 15:57:48 +0000

    modify MiddleLetter class to pass second test

--- middle_letters/app/src/main/java/middle_letters/MiddleLetter.java
+++ middle_letters/app/src/main/java/middle_letters/MiddleLetter.java
@@ -2,13 +2,13 @@ package middle_letters;
 
 public class MiddleLetter {
     public String get_middle(String word) {
-        int startPoint = word.length()/2 -1 ;
+        int startPoint = word.length()/2;
         StringBuilder middle = new StringBuilder();
+        if (word.length() % 2 == 0) {
+            middle.append(word.charAt(startPoint - 1));
+        }
         middle.append(word.charAt(startPoint));
 
-        if (word.length() % 2 == 0) {
-            middle.append(word.charAt(startPoint + 1));
-        }
         return middle.toString();
     }
 }
--- middle_letters/app/src/test/java/middle_letters/MiddleLetterTest.java
+++ middle_letters/app/src/test/java/middle_letters/MiddleLetterTest.java
@@ -14,4 +14,8 @@ public class MiddleLetterTest {
     @Test public void middleOfTest() {
         assertEquals("middle of test is es", middleLetter.get_middle("test"), String.valueOf("es"));
     }
+
+    @Test public void middleOfTesting() {
+        assertEquals("middle of testing is t", middleLetter.get_middle("testing"), String.valueOf("t"));
+    }
 }
