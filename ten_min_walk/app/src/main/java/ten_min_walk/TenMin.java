d09bc4ec6eac07bccb002225d89851cabc657e67
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-16 21:53:33 +0000

    all ten minute walk tests pass

--- ten_min_walk/app/src/main/java/ten_min_walk/TenMin.java
+++ ten_min_walk/app/src/main/java/ten_min_walk/TenMin.java
@@ -1,7 +1,29 @@
 package ten_min_walk;
 
+import java.util.Objects;
+
 public class TenMin {
     public Boolean tenMinuteWalk(String[] walk) {
-        return true;
+        Integer north = 0;
+        Integer south = 0;
+        Integer east = 0;
+        Integer west = 0;
+
+        if (walk.length < 10) {
+            return false;
+        }
+
+        for (String item : walk) {
+            if (Objects.equals(item, "n")) {
+                north ++;
+            } else if (Objects.equals(item, "s")) {
+                south ++;
+            } else if (Objects.equals(item, "e")) {
+                east ++;
+            } else if (Objects.equals(item, "w")) {
+                west ++;
+            }
+        }
+        return (Objects.equals(north, south)) && (Objects.equals(east, west));
     }
 }
--- ten_min_walk/app/src/test/java/ten_min_walk/TenMinTest.java
+++ ten_min_walk/app/src/test/java/ten_min_walk/TenMinTest.java
@@ -12,6 +12,21 @@ public class TenMinTest {
     }
     @Test public void testOne() {
         String[] testWalk = { "w", "s", "e", "e", "n", "n", "e", "s", "w", "w" };
-        assertTrue("app should have a greeting", tenMin.tenMinuteWalk(testWalk));
+        assertTrue("a ten minute walk", tenMin.tenMinuteWalk(testWalk));
+    }
+
+    @Test public void testTwo() {
+        String[] testWalk = { "w", "s", "e", "n", "n", "e", "s", "w", "w", "w" };
+        assertFalse("a ten minute walk", tenMin.tenMinuteWalk(testWalk));
+    }
+
+    @Test public void testThree() {
+        String[] testWalk = { "w", "s", "e", "s", "s", "e", "s", "w", "n", "n" };
+        assertFalse("a ten minute walk", tenMin.tenMinuteWalk(testWalk));
+    }
+
+    @Test public void testFour() {
+        String[] testWalk = { "w", "s" };
+        assertFalse("a ten minute walk", tenMin.tenMinuteWalk(testWalk));
     }
 }
