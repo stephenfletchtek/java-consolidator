b392ad1b98b7da34c5dbddffec8688cb1b0bead6
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-17 17:01:40 +0000

    refactor to calculate price

--- checkout/app/src/main/java/checkout/Checkout.java
+++ checkout/app/src/main/java/checkout/Checkout.java
@@ -3,13 +3,47 @@ package checkout;
 import java.util.Objects;
 
 public class Checkout {
+    private Integer[] A;
+    private Integer[] B;
+    private Integer[] C;
+    private Integer[] D;
+
+    public Checkout() {
+        A = new Integer[] {50, 3, 130};
+        B = new Integer[] {30, 2, 45};
+        C = new Integer[] {20};
+        D = new Integer[] {15};
+    }
     public Integer checkout(Integer basket) {
         return -1;
     }
     public Integer checkout(String basket) {
-        if (Objects.equals(basket, "AA")) {
-            return 100;
+        Integer total = 0;
+        if (!validBasket(basket)) {
+            return -1;
         }
-        return -1;
+        
+        total = countItem(basket, "A") * A[0];
+
+        return total;
+    }
+
+    private Integer countItem(String basket, String item) {
+        Integer count = 0;
+        for (int i = 0; i < basket.length(); i++ ) {
+            if (basket.charAt(i) == item.charAt(0)) {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    private Boolean validBasket(String basket) {
+        for (int i = 0; i < basket.length(); i++) {
+            if ("ABCD".indexOf(basket.charAt(i)) == -1 ) {
+                return false;
+            }
+        }
+        return true;
     }
 }
