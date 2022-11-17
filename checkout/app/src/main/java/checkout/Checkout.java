10fdb01f45010349465593c596a5b76e4cec397b
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-17 15:38:26 +0000

    1st test passes hardcoded

---
+++ checkout/app/src/main/java/checkout/Checkout.java
@@ -0,0 +1,7 @@
+package checkout;
+
+public class Checkout {
+    public Integer checkout(String basket) {
+        return -1;
+    }
+}
--- checkout/app/src/test/java/checkout/CheckoutTest.java
+++ checkout/app/src/test/java/checkout/CheckoutTest.java
@@ -1,11 +1,14 @@
 package checkout;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CheckoutTest {
-    @Before public void initialize() {
-        Shop shop = new Shop();
+    Checkout shop;
+    @Before
+    public void initialize() {
+        shop = new Checkout();
     }
     @Test public void testOne() {
         assertEquals("aBc", shop.checkout("aBc"), Integer.valueOf(-1));
