a361549cb5cb98cd714121e5999107d744c4e061
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-17 13:50:47 +0000

    refactor code for scores;

--- scrabble_solver/app/src/main/java/scrabble_solver/Scrabble.java
+++ scrabble_solver/app/src/main/java/scrabble_solver/Scrabble.java
@@ -4,14 +4,35 @@ import java.util.Objects;
 
 public class Scrabble {
     String word;
+    String onePoint = "AaEeIiOoUuLlNnRrSsTt";
+    String twoPoints = "DdGg";
+    String threePoints = "BbCcMmPp";
+    String fourPoints = "FfHhVvWwYy";
+    String fivePoints = "Kk";
+    String eightPoints = "JjXx";
+    String tenPoints = "QqZz";
+    Integer score;
+
     public Scrabble(String word) {
         this.word = word;
+        this.score = 0;
     }
 
     public Integer score() {
-        if (Objects.equals(this.word, "a")) {
-            return 1;
+        calcScore(1, onePoint);
+        return this.score;
+    }
+
+    private void calcScore(Integer amount, String group) {
+        int limit = 0;
+        if (this.word != null) {
+            limit = this.word.length();
         }
-        return 0;
+        for (int i = 0; i < limit; i++) {
+            char chr = this.word.charAt(i);
+            if (group.indexOf(chr) != -1) {
+                this.score += amount;
+            }
+        }
     }
 }
