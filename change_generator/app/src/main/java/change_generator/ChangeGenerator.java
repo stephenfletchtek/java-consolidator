bc50b86031df4ddf03b5906fb007947043c09985
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-18 10:34:52 +0000

    first two tests pass with refactored code

--- change_generator/app/src/main/java/change_generator/ChangeGenerator.java
+++ change_generator/app/src/main/java/change_generator/ChangeGenerator.java
@@ -1,12 +1,27 @@
 package change_generator;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class ChangeGenerator {
+    String[] divisors;
+    String[] output;
+    ArrayList<String> toReturn;
+
+    public ChangeGenerator() {
+        divisors = new String[] { "20", "5", "1", "0.2", "0.02"};
+        output = new String[] { "£20", "£5", "£1", "20p", "2p"};
+        toReturn = new ArrayList<>();
+    }
     public String[] convert(BigDecimal amount) {
-        if (amount.equals(new BigDecimal(40))) {
-            return new String[] {"£20", "£20"};
+        for (int j = 0; j < divisors.length; j++) {
+            BigDecimal bdDivisor = new BigDecimal(divisors[j]);
+            BigDecimal[] res = amount.divideAndRemainder(bdDivisor);
+            for (int i = 0; i < res[0].intValue(); i++ ) {
+                toReturn.add(output[j]);
+            }
+            amount = res[1];
         }
-        return new String[] {"£5", "£1", "20p", "20p", "2p"};
+        return toReturn.toArray(new String[0]);
     }
 }
