ccacce5b19e67064fe4039a1e0e3030f8f5ec190
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-18 14:48:06 +0000

    all 5 tests passing after refactor

--- change_generator/app/src/main/java/change_generator/ChangeGenerator.java
+++ change_generator/app/src/main/java/change_generator/ChangeGenerator.java
@@ -5,23 +5,29 @@ import java.util.ArrayList;
 
 public class ChangeGenerator {
     String[] divisors;
-    String[] output;
     ArrayList<String> toReturn;
 
     public ChangeGenerator() {
         divisors = new String[] {"50", "20", "10", "5", "2", "1", "0.5", "0.2", "0.1", "0.05", "0.02", "0.01"};
-        output = new String[] {"£50", "£20", "£10", "£5", "£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p"};
         toReturn = new ArrayList<>();
     }
     public String[] convert(BigDecimal amount) {
-        for (int j = 0; j < divisors.length; j++) {
-            BigDecimal divisor = new BigDecimal(divisors[j]);
+        for (String str : divisors) {
+            BigDecimal divisor = new BigDecimal(str);
             BigDecimal[] res = amount.divideAndRemainder(divisor);
             for (int i = 0; i < res[0].intValue(); i++) {
-                toReturn.add(output[j]);
+                toReturn.add(formatOutput(str));
             }
             amount = res[1];
         }
         return toReturn.toArray(new String[0]);
+    }
+
+    private String formatOutput(String input) {
+        if (input.contains(".")) {
+            BigDecimal result = new BigDecimal(input).multiply(new BigDecimal(100));
+            return result.intValue() + "p";
+        }
+        return "£" + Integer.valueOf(input);
     }
 }
