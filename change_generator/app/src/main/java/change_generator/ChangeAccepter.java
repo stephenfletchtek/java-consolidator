e9e02f4ce78f7ccbcf3fa160a2d4ddf6edd03be6
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-11-18 19:29:49 +0000

    two tests passing after refactor

--- change_generator/app/src/main/java/change_generator/ChangeAccepter.java
+++ change_generator/app/src/main/java/change_generator/ChangeAccepter.java
@@ -1,15 +1,33 @@
 package change_generator;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
+import java.math.RoundingMode;
 
 public class ChangeAccepter {
+    BigDecimal total;
+
+    public ChangeAccepter() {
+        total = new BigDecimal(0);
+    }
     public String deposit(String[] input) {
-        if (Arrays.equals(input, new String[]{"£20", "£20"})) {
-         return "£40.00";
+        for (String item : input) {
+            total = total.add(toBigDecimal(item));
         }
-        return "£6.42";
+        return formatOutput(total, "£");
+    }
+
+    private BigDecimal toBigDecimal(String input) {
+        String numbers = "1234567890";
+        char chr = input.charAt(0);
+        String first = Character.toString(chr);
+        if (!numbers.contains(first)) {
+            return new BigDecimal(input.substring(1));
+        }
+        return new BigDecimal(input.substring(0, input.length() - 1)).divide(new BigDecimal(100));
+    }
+
+    private String formatOutput(BigDecimal total, String currency) {
+        total = total.setScale(2, RoundingMode.HALF_UP);
+        return currency + total.toString();
     }
 }
